311be1ab8621eb6393a1c3a48d45427565e830bb
AlexBanPer <alejandromartpv@icloud.com>
2017-10-05 18:40:54 -0300

    Gestion de Base de datos
    Se inserto el codigo para establecer conexion con la base de datos.

--- conexion.php
+++ conexion.php
@@ -1,3 +1,35 @@
 <!-- Conexión a la base de datos,
 codificación de caracteres,
 seleccion de base de datos. -->
+<?php 
+
+/**
+* PDOConnect
+* Nota: La seleccion de la base de datos se hara por cada sentencia.
+*/
+class PDOConnect
+{
+	protected $dbPDO;
+	private $tableName;
+
+	function __construct($dbhost, $dbname, $dbuser, $dbpass)
+	{
+		try{
+			//Creamos la base de datos en PDO
+			$this->dbPDO = new PDO('mysql:host='.$dbhost.';dbname='.$dbname, $dbuser, $dbpass);
+			//echo "connected!"; //Si esta conectado, mostrara un mensaje.
+		}catch(PDOException $e){
+			print "Error!: ".$e->getMessage()."<br>"; // En caso de error, nos dira cual es.
+			die(); // Terminar todos los procesos de la clase.
+		}
+		
+	}
+
+	private function doConn(){
+		
+	}
+}
+
+
+
+ ?>
