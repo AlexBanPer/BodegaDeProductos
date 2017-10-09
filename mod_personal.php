17c15290b40a0b8603b113a2dbc2c312bb5ebb09
AlexBanPer <alejandromartpv@icloud.com>
2017-10-09 07:23:17 -0300

    Modificar Personal
    Se crea el modificar personal ademas de un extra cual actualiza la sesion en caso de que el usuario se modifique a si mismo siendo un NO Super Admin.

--- mod_personal.php
+++ mod_personal.php
@@ -1,121 +1,154 @@
+<?php 
+//Recuperamos, para incluir el archivo sesion.php & iniciamos la variable.
+include('sesion.php');
+$data = Sessions::getInstance();
 
-<!-- Inclución de archivos requeridos -->
-
+if ($data->isOnline == false) {
+	header("Location: login.php");
+}
+?>
 <!DOCTYPE html> 
 <html>
-	<head>
-		<meta charset="UTF-8"/>
-		<title>Modificar personal</title>
-		<link type="text/css" href="estilo.css" rel="stylesheet">
+<head>
+	<meta charset="UTF-8"/>
+	<title>Modificar personal</title>
+	<link type="text/css" href="estilo.css" rel="stylesheet">
 
-	</head>
+</head>
 
-	<body>
-		
-		<div class="contenedor">
-			<div class= "encabezado">
-				<div class="izq">
-			
-				<p>Bienvenido/a:<br><!-- Agregar variable de sesión con nombre y apellido del usuario --></p>
+<body>
 
-				</div>
+	<div class="contenedor">
+		<div class= "encabezado">
+			<div class="izq">
 
-				<div class="centro">
-					<a href=principalAdmin.php><center><img src='imagenes/home.png'><br>Home<center></a> 
-				</div>
-				
-				<div class="derecha">
-					<a href="salir.php?sal=si"><img src="imagenes/cerrar.png"><br>Salir</a>
-				</div>
+				<p>Bienvenido/a:<br><?php echo $_SESSION['fullname']; ?></p>
+
 			</div>
-			<br><h1 align='center'>REGISTROS EXISTENTES</h1><br>
-			<?php
-				include('conexion.php');
 
-				$consulta="SELECT * FROM personal";
-				$ejecutar=mysql_query($consulta,$conexion);
-			
-				echo "<table  width='80%' align='center'><tr>";	         	  
-				echo "<th width='20%'>RUT</th>";
-				echo "<th width='20%'>NOMBRE</th>";
-				echo "<th width='20%'>APELLIDO</th>";
-				echo "<th width='20%'>CARGO</th>";
-				echo  "</tr>"; 
-			
-				while($result=mysql_fetch_array($ejecutar)){	
-		          	
-		          echo "<tr>";	         	  
-				  echo '<td width=20%>'.$result['rut'].'</td>';
-				  echo '<td width=20%>'.$result['nombre'].'</td>';
-				  echo '<td width=20%>'. $result['apellido'].'</td>';
-				  echo '<td width=20%>'.$result['cargo'].'</td>';
-				  echo "</tr>";
-				}
-				 echo "</table></br>";
-			?>
+			<div class="centro">
+				<a href=principalAdmin.php><center><img src='imagenes/home.png'><br>Home<center></a> 
+			</div>
 
-
-			<div class="encabezado">
-	            <h1>Modificar personal</h1>
-	        </div>
-
-	        <div class="formulario">
-	            <form ="registro" method="post" action="" enctype="application/x-www-form-urlencoded">
-
-	                <div class="campo">
-	                    <label name="Seleccionar">Ingresa el Rut del registro a modificar:</label>
-			 			<input name='seleccionar' type="text" required>
-	                </div>
-
-	                <div class="campo">
-	                    <div class="en-linea izquierdo">
-	                        <label for="nombre">Nombre:</label>
-	                        <input type="text" name="nombre" required/>
-	                    </div>
-
-	                    <div class="en-linea">
-	                        <label for="apellido">Apellido:</label>
-	                        <input type="text" name="apellido" required/>
-	                    </div>
-	                </div>
-
-	                <div class="campo">
-	                    <label for="cargo">cargo:</label>
-		                <select name="cargo" required/>
-		                    <option>Admin</option>
-		                    <option>Bodega</option>
-	                    </select>
-	                </div>
-
-	                <div class="botones">
-	                    <input type="submit" name="modificar" value="Modificar"/>
-					</div>
-				</form>
-				<?php
-					
-				    // La siguiente línea verifica que la varible del boton submit "modificar" este creada.	
-					if (isset($_POST['modificar'])) {
-
-						//La siguiente linea recupera la variable donde se ingreso el rut a modificar.
-						$seleccionar = $_POST['seleccionar'];
-
-						// Las siguientes 2 líneas verifican que el registro que se desea modificar no corresponda al rut del Admin y se muestra alerta con mensaje. 
-						if ($seleccionar == '180332403') {
-							echo "<script lenguaje='javascript'>alert('Admin general no puede ser modificado');</script>";
-						}else {
-
-							/* Si no corresponde al rut del Admin entonces:
-							 		Recuperar las variables con los datos ingresados.
-							 		Realizar la actualización de los datos.
-									Redirigir el flujo a esta misma página para visualizar los cambios */ 
-							
-						};
-
-						
-					};
-						 
-				?>
+			<div class="derecha">
+				<a href="salir.php?sal=si"><img src="imagenes/cerrar.png"><br>Salir</a>
 			</div>
 		</div>
-	</body>
+		<br><h1 align='center'>REGISTROS EXISTENTES</h1><br>
+		<?php
+
+		$dbPDOClass = new PDOConnect();
+		$stmt = $dbPDOClass->dbPDO->prepare("SELECT * FROM personal");
+		$stmt->execute();
+		$dbresult = $stmt->fetchAll();
+
+		echo "<table  width='80%' align='center'><tr>";	         	  
+		echo "<th width='20%'>RUT</th>";
+		echo "<th width='20%'>NOMBRE</th>";
+		echo "<th width='20%'>APELLIDO</th>";
+		echo "<th width='20%'>CARGO</th>";
+		echo  "</tr>"; 
+
+		foreach ($dbresult as $result){	
+
+			echo "<tr>";	         	  
+			echo '<td width=20%>'.$result['rut'].'</td>';
+			echo '<td width=20%>'.$result['nombre'].'</td>';
+			echo '<td width=20%>'. $result['apellido'].'</td>';
+			echo '<td width=20%>'.$result['cargo'].'</td>';
+			echo "</tr>";
+		}
+		echo "</table></br>";
+		?>
+
+
+		<div class="encabezado">
+			<h1>Modificar personal</h1>
+		</div>
+
+		<div class="formulario">
+			<form ="registro" method="post" action="" enctype="application/x-www-form-urlencoded">
+
+				<div class="campo">
+					<label name="Seleccionar">Ingresa el Rut del registro a modificar:</label>
+					<input name='seleccionar' type="text" required>
+				</div>
+
+				<div class="campo">
+					<div class="en-linea izquierdo">
+						<label for="nombre">Nombre:</label>
+						<input type="text" name="nombre" required/>
+					</div>
+
+					<div class="en-linea">
+						<label for="apellido">Apellido:</label>
+						<input type="text" name="apellido" required/>
+					</div>
+				</div>
+
+				<div class="campo">
+					<label for="cargo">cargo:</label>
+					<select name="cargo" required/>
+					<option>Admin</option>
+					<option>Bodega</option>
+				</select>
+			</div>
+
+			<div class="botones">
+				<input type="submit" name="modificar" value="Modificar"/>
+			</div>
+		</form>
+		<?php
+		if (isset($_POST['modificar']))
+		{
+			$seleccionar = $_POST['seleccionar'];
+			if ($seleccionar == '2048371045')
+			{
+				echo "<script lenguaje='javascript'>alert('Admin general no puede ser modificado');</script>";
+			}
+			else
+			{
+				$validation = $dbPDOClass->dbPDO->prepare("SELECT rut FROM personal WHERE rut=:rut");
+				$validation->bindParam(':rut', $seleccionar, PDO::PARAM_STR);
+				$validation->execute();
+				$count = $validation->rowCount();
+				if($count == 0)
+				{
+					echo "<script lenguaje='javascript'>alert('Usuario no encontrado!');</script>";
+				}
+				else
+				{
+
+					//Validacion auxiliar para no perder los usuarios.
+					$nombre = $_POST['nombre'];
+					$updatename = (empty($nombre)) ? $_SESSION['name'] : $nombre;
+
+					$apellido = $_POST['apellido'];
+					$updateapellido = (empty($apellido)) ? $_SESSION['apellido'] : $apellido;
+
+					$cargo = $_POST['cargo'];
+					$updatecargo = (empty($cargo)) ? $_SESSION['cargo'] : $cargo;
+
+					$update = $dbPDOClass->dbPDO->prepare("UPDATE personal SET nombre=:nombre, apellido=:apellido, cargo=:cargo WHERE rut=:rut");
+					$update->bindParam(':nombre', $updatename, PDO::PARAM_STR);
+					$update->bindParam('apellido', $updateapellido, PDO::PARAM_STR);
+					$update->bindParam('cargo', $updatecargo, PDO::PARAM_STR);
+					$update->bindParam('rut', $seleccionar, PDO::PARAM_STR);
+					$update->execute();
+					echo "<script lenguaje='javascript'>alert('Usuario actualizado!');</script>";
+
+					//El siguiente codigo fue un agregado mio, para actualizar la sesion en caso de que actualice su propio usuario.
+					if ($seleccionar == $_SESSION['rut']) {
+						header("Location: updatesession.php?name=".$updatename."&apellido=".$updateapellido."&&cargo=".$cargo);
+					}else{
+						header("Location: mod_personal.php");
+					}
+				}
+			};
+
+		};
+		?>
+	</div>
+</div>
+</body>
 </html>		
